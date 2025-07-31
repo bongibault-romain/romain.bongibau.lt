1b528d1fb6f514f75c139b96968c6c96fc6b47c0
Bongibault Romain <bongibault.romain@gmail.com>
2025-07-31 12:57:50 +0200

    Refactor code structure for improved readability and maintainability

--- components/Education.tsx
+++ components/Education.tsx
@@ -1,60 +1,45 @@
-const Svg1 = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16">
-    <path
-      fill="#9C76EC"
-      d="M7.567 7.6c-1.814-.137-3.56-.885-4.874-2.204A7.662 7.662 0 0 1 .5.5c1.868.136 3.56.884 4.874 2.203A7.662 7.662 0 0 1 7.567 7.6Zm.865 0c1.814-.137 3.56-.885 4.873-2.204C14.62 4.076 15.364 2.322 15.5.5c-1.869.136-3.629.884-4.874 2.203A7.662 7.662 0 0 0 8.432 7.6Zm4.873 3.005c-1.313-1.32-3.06-2.068-4.873-2.204a7.662 7.662 0 0 0 2.193 4.896c1.313 1.32 3.06 2.067 4.874 2.204-.136-1.877-.88-3.645-2.194-4.896ZM7.636 8.4c-1.882.136-3.629.884-4.942 2.204C1.38 11.854.622 13.623.5 15.5c1.868-.123 3.56-.884 4.942-2.204a7.662 7.662 0 0 0 2.193-4.896Z"
-    />
-  </svg>
-);
+import Image from "next/image";
 
-const Svg2 = () => (
-  <svg
-    className="fill-gray-700 dark:fill-gray-100"
-    xmlns="http://www.w3.org/2000/svg"
-    width="18"
-    height="11"
-  >
-    <path d="M5.077 0c2.803 0 5.076 2.418 5.076 5.4s-2.273 5.4-5.076 5.4C2.273 10.8 0 8.382 0 5.4S2.273 0 5.077 0Zm8.106.316c1.402 0 2.538 2.276 2.538 5.084 0 2.807-1.136 5.084-2.538 5.084S10.645 8.207 10.645 5.4c0-2.807 1.136-5.084 2.538-5.084ZM18 5.4c0-2.515-.4-4.554-.893-4.554-.492 0-.892 2.04-.892 4.554 0 2.515.4 4.555.892 4.555.494 0 .893-2.04.893-4.555Z" />
-  </svg>
-);
 
-const Svg3 = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="18" height="16">
-    <path
-      fill="#00ADEF"
-      d="M17.995 3.602c-.075 1.725-1.274 4.124-3.674 7.198-2.474 3.15-4.499 4.799-6.223 4.799-1.05 0-1.95-.975-2.7-2.924-.45-1.8-.974-3.524-1.424-5.324-.525-1.95-1.125-2.924-1.725-2.924-.15 0-.6.3-1.424.825L0 4.202c.9-.75 1.8-1.575 2.624-2.325 1.2-1.05 2.1-1.574 2.7-1.65 1.424-.15 2.249.826 2.549 2.85.375 2.175.6 3.6.75 4.124.375 1.8.825 2.774 1.35 2.774.374 0 .974-.6 1.724-1.8.75-1.199 1.125-2.099 1.2-2.698.075-1.05-.3-1.575-1.2-1.575-.45 0-.9.075-1.35.3.9-2.924 2.55-4.274 5.099-4.199 1.8.075 2.624 1.275 2.55 3.599Z"
-    />
-  </svg>
-);
+const INSA = () => (
+  <Image src="/images/insa.jpg" className="rounded-full" alt="INSA Toulouse" width={24} height={24} />
+)
+
+const Faidherbe = () => (
+  <Image src="/images/faidherbe.jpg" className="rounded-full" alt="INSA Toulouse" width={24} height={24} />
+)
+
+const ISP = () => (
+  <Image src="/images/isp.png" className="rounded-full" alt="INSA Toulouse" width={24} height={24} />
+)
 
 export default function Education() {
   const items = [
     {
-      title: "Senior Developer and VP of Product at Qonto",
-      link: "#0",
-      icon: <Svg1 />,
-      date: "2021 - Today",
-      location: "London, UK",
+      title: "Diplôme d'ingénieur en informatique et réseaux",
+      link: "https://www.insa-toulouse.fr/formation/ingenieur-specialite-informatique-et-reseaux/",
+      icon: <INSA />,
+      date: "2023 - 2026",
+      location: "Institut National des Sciences Appliquées, 31400 Toulouse",
       description:
-        "Setting vision, leadership, processes while directly participating in communications with clients such as eBay, Amazon, X, Twitch, Instagram, and many others.",
+        "Acquisition de compétences approfondies en mathématiques, en optimisation, en réseaux et en algorithmique.",
     },
     {
-      title: "Fullstack Developer at Medium Inc.",
-      link: "#0",
-      icon: <Svg2 />,
-      date: "2019 - 2021",
-      location: "New York, NYC",
-      description:
-        "Setting vision, leadership, processes while directly participating in communications with clients such as eBay, Amazon, X, Twitch, Instagram, and many others.",
+      title: "Classe Préparatoire Aux Grandes Ecoles",
+      link: "https://www.faidherbe.org/",
+      icon: <Faidherbe />,
+      date: "2021 - 2023",
+      location: "Lycée Faidherbe, 59034 Lille",
+      description: "Filière Mathématiques, Physique et Informatique (MPI), développant mes compétences analytiques et techniques dans ces domaines."
     },
     {
-      title: "System Engineer and Web Developer at Vimeo",
-      link: "#0",
-      icon: <Svg3 />,
-      date: "2017 - 2019",
-      location: "London, UK",
+      title: "Baccalauréat Général",
+      link: "https://www.ispfourmies.com/",
+      icon: <ISP />,
+      date: "2018 - 2021",
+      location: "Institution Saint Pierre, 59610 Fourmies",
       description:
-        "Setting vision, leadership, processes while directly participating in communications with clients such as eBay, Amazon, X, Twitch, Instagram, and many others.",
+      "Obtention avec mention Bien, spécialisé en mathématiques, physique et SVT avec option mathématiques expertes."
     },
   ];
 
@@ -81,6 +66,7 @@ export default function Education() {
                   <h3 className="font-semibold text-gray-800 dark:text-gray-100">
                     <a
                       className="hover:underline decoration-2 decoration-gray-300 dark:decoration-gray-600 underline-offset-2"
+                      target="_blank"
                       href={item.link}
                     >
                       {item.title}
