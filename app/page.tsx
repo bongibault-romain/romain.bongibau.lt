47381f956b43532cba294d111453b48e508d1392
Bongibault Romain <bongibault.romain@gmail.com>
2025-07-31 12:28:24 +0200

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,25 @@
+import Tutorials from "@/components/Tutorials";
+import Experience from "@/components/Experience";
+import Articles from "@/components/Articles";
+import SideHustles from "@/components/SideHustles";
+import Recommendations from "@/components/Recommendations";
+import Newsletter from "@/components/Newsletter";
+import Education from "@/components/Education";
+import Projects from "@/components/Projects";
+
+export const metadata = {
+  title: "Home - DevFolio",
+  description: "Page description",
+};
+
+export default async function Home() {
+  return (
+    <>
+      <Experience />
+      <Projects />
+      <Education />
+      <Recommendations />
+      <Articles />
+    </>
+  );
+}
