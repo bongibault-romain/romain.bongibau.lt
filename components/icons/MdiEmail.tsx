8f71463c706469dd151e1e687bb3061b16943158
Bongibault Romain <bongibault.romain@gmail.com>
2025-07-31 15:01:32 +0200

    Add MdiEmail and MdiGithub icon components

---
+++ components/icons/MdiEmail.tsx
@@ -0,0 +1,8 @@
+import React, { SVGProps } from 'react'
+
+export function MdiEmail(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}>{/* Icon from Material Design Icons by Pictogrammers - https://github.com/Templarian/MaterialDesign/blob/master/LICENSE */}<path fill="currentColor" d="m20 8l-8 5l-8-5V6l8 5l8-5m0-2H4c-1.11 0-2 .89-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2" /></svg>
+  )
+}
+export default MdiEmail
