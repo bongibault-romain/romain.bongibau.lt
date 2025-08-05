9f7da253c7db0321602a4b7b9c0bfdb2ce809903
Bongibault Romain <bongibault.romain@gmail.com>
2025-08-05 08:58:32 +0200

    Add sitemap generation functionality with image URLs

---
+++ app/sitemap.ts
@@ -0,0 +1,17 @@
+import type { MetadataRoute } from 'next'
+ 
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: process.env.NEXT_PUBLIC_SITE_URL!,
+      lastModified: '2025-08-05',
+      changeFrequency: 'weekly',
+      priority: 0.5,
+      images: [
+        `${process.env.NEXT_PUBLIC_SITE_URL!}/images/header-image-01.jpg`,
+        `${process.env.NEXT_PUBLIC_SITE_URL!}/images/header-image-02.jpg`,
+        `${process.env.NEXT_PUBLIC_SITE_URL!}/images/header-image-03.jpg`,
+      ],
+    },
+  ]
+}
